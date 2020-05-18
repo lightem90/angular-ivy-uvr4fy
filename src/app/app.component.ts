d143353e041d8ff1a0db3fbd00456a961447fbce
lightem90 <matteo.liera@gmail.com>
2020-05-18 13:54:02 +0200

    corretto boilerplate, angularfire2 rimosso (perchè deprecato), creato modulo apposito per firebase e aggiornato

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { FirebaseApp } from '@angular/fire';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,7 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  constructor(public app: FirebaseApp) {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,6 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
-import * as firebase from 'firebase/app';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFireStorageModule } from '@angular/fire/storage';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -22,17 +17,14 @@ import { AboutComponent } from './static/about/about.component';
 import { FaqComponent } from './static/faq/faq.component';
 import { ContactsComponent } from './static/contacts/contacts.component';
 
-import { environment } from './environments/environment';
 import { AccountService } from './core/service/account.service';
 import { FirebaseHelper } from './core/service/firebase-helper';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material.module';
+import { FirebaseModule } from './firebase.module';
 
 
-if (!firebase.apps.length) {
-    firebase.initializeApp(environment.firebase);
-}
 
 export function translateHttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -51,10 +43,7 @@ export function translateHttpLoaderFactory(http: HttpClient) {
     BrowserModule, 
     FormsModule, 
     ReactiveFormsModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireStorageModule,
-    AngularFireAuthModule,
-    AngularFireDatabaseModule,
+    FirebaseModule,
     AppRoutingModule, 
     MaterialModule],
   declarations: [ 
