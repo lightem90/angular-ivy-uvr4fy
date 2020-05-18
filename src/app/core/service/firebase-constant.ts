56cefa7597757aa6c76641f3e9703b1fd8a55b98
lightem90 <matteo.liera@gmail.com>
2020-05-18 11:49:56 +0200

    documentation for relationship

--- src/app/core/service/firebase-constant.ts
+++ src/app/core/service/firebase-constant.ts
@@ -1,13 +1,20 @@
 export class FirebaseConstant {
 
   static entityTableNames = {
-    user : "app-user",
-    mission: "app-mission" //key is user, value is list of owned missions
+    user: "app-user",       //list of users with their data (if any)
+    mission: "app-mission"  //list of confirmed missions with data and ids
   }
 
   static relationTableNames = {
-    userRole : "user-role",
-    userMission: "user-mission"  //key is mission, value is list of partecipants
+    userMission: "mission-partecipants",    //key is mission, value is list of partecipant uids
+    missionOwnership: "user-mission"        //key is user, value is list of owned mission uids
   }
+
+  //OPERATION
+  //A) Adding/Removing mission
+  //1- New entry in app-mission 2- new entry in user mission 3- new entry in mission-part (owner must partecipates)
+  //B) Joining/Leaving mission
+  //1- Add entry (joining user id) in userMission for selected mission
+  //C) TODO feedback system for user / mission
   
 }
