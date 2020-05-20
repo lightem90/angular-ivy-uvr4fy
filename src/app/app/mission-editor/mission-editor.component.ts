0a0c02d4cf52f4a61226e6072f1d70a7aa0b450b
lightem90 <matteo.liera@gmail.com>
2020-05-20 19:46:42 +0200

    wip geocoder

--- src/app/app/mission-editor/mission-editor.component.ts
+++ src/app/app/mission-editor/mission-editor.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import mapboxgl = require('mapbox-gl');
+
+import { environment } from '../../../environments/environment';
 @Component({
   selector: 'app-mission-editor',
   templateUrl: './mission-editor.component.html',
@@ -13,15 +17,21 @@ export class MissionEditorComponent implements OnInit {
   constructor(
     private _fb : FormBuilder) { 
 
-  this.missionForm = _fb.group({
-    missionName: ["", [	
-        Validators.maxLength(256),
-        Validators.required]
-      ],
-    missionDate: [new Date(), [
-      Validators.required
-    ]]
-  })
+    this.missionForm = _fb.group({
+      missionName: ["", [	
+          Validators.maxLength(256),
+          Validators.required]
+        ],
+      missionDate: [new Date(), [
+        Validators.required
+      ]]
+    })
+
+    const geocoder = new MapboxGeocoder({
+      accessToken: environment.mapbox.accessToken,
+      mapboxgl : mapboxgl
+    })
+
   }
 
   ngOnInit() {
---
+++ src/app/core/service/mapbox-helper.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+import { environment } from '../../../environments/environment';
+
+import mapboxgl = require('mapbox-gl');
+import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+@Injectable()
+export class MapboxHelper
+{
+
+}
